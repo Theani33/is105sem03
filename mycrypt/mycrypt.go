943bcc29549c2899513348c0909a16edc31923be
Theani33 <theani@uia.no>
2023-05-03 00:11:36 +0200

    l

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,26 +1,28 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; KSN")
 
-func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
-	kryptertMelding := make([]rune, len(melding))
-	for i := 0; i < len(melding); i++ {
-		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
-		} else {
-			kryptertMelding[i] = alphabet[indeks+chiffer]
-		}
-	}
-	return kryptertMelding
+func Krypter(input []rune, key []rune, chiffer int) []rune {
+    output := make([]rune, len(input))
+    for i, r := range input {
+        index := sokIAlfabetet(r, key)
+        if index < 0 {
+            // character not found in key
+            output[i] = r
+        } else {
+            encryptedIndex := (index + chiffer) % len(key)
+            output[i] = key[encryptedIndex]
+        }
+    }
+    return output
 }
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
-	for i := 0; i < len(alfabet); i++ {
-		if symbol == alfabet[i] {
-			return i
-			break
-		}
-	}
-	return -1
+    for i := 0; i < len(alfabet); i++ {
+        if symbol == alfabet[i] {
+            return i
+            break
+        }
+    }
+    return -1
 }
